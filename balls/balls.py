e83da9b07a18b2dc19e4709ad60d43edc4777b34
denballakh <47365157+denballakh@users.noreply.github.com>
2020-10-19 20:28:59 +0300

    Add files via upload
    Исправил ошибки, изменил формулу расчета траектории квадратов

--- balls/balls.py
+++ balls/balls.py
@@ -1,7 +1,12 @@
-import pygame
-from pygame.draw import *
-from random import randint
-import math
+import pygame as py
+import pygame.gfxdraw
+import pygame.locals
+import pygame.font
+import random
+from math import pi, sin, cos
+
+randomReal = random.uniform
+randomInt = random.randint
 
 def vecSum(v1, v2):
     '''
@@ -10,7 +15,7 @@ def vecSum(v1, v2):
     Результат:
     [v1x + v2x, v1y + v2y]
     '''
-    return [v1[0]+v2[0], v1[1]+v2[1]]
+    return v1[0]+v2[0], v1[1]+v2[1]
 
 def vecMul(v, k):
     '''
@@ -20,22 +25,14 @@ def vecMul(v, k):
     Результат:
     [vx * k, vy * k]
     '''
-    return [v[0]*k, v[1]*k]
-
-def randomReal(min=0.0, max=1.0, n=1e10):
-    '''
-    Возвращает случайное вещественное число между min и max
-    min, max - вещественные числа (по умолчанию - 0.0, 1.0)
-    n - число возможных чисел между min и max (по умолчанию - 1e10)
-    '''
-    return min + randint(0,int(n))/int(n) * (max - min)
+    return v[0]*k, v[1]*k
 
 def randomColor(colors):
     '''
     Возвращает случайный цвет из массива colors
     '''
-    return colors[randint(0, len(colors) - 1)]
-    #return (randint(0, 255), randint(0, 255), randint(0, 255))
+    return colors[randomInt(0, len(colors) - 1)]
+    #return (randomInt(0, 255), randomInt(0, 255), randomInt(0, 255))
 
 def randomVelocity(vel_diap=None, ang_diap=None):
     '''
@@ -44,9 +41,9 @@ def randomVelocity(vel_diap=None, ang_diap=None):
         Направление скорости из диапазона ang_diap (по умолчанию [0, 2*PI])
     '''
     if vel_diap == None:
-        vel_diap = [V_MIN, V_MAX]
+        vel_diap = V_MIN, V_MAX
     if ang_diap == None:
-        ang_diap = [0, 2*PI]
+        ang_diap = 0, 2*pi
 
     ang_min, ang_max = ang_diap
     vel_min, vel_max = vel_diap
@@ -54,23 +51,26 @@ def randomVelocity(vel_diap=None, ang_diap=None):
     vel = randomReal(vel_min, vel_max)
     vx = vel * cos(ang)
     vy = vel * sin(ang)
-    return [vx, vy]
+    return vx, vy
 
 def randomBallState():
     '''
     Возвращает шарик со случайным состоянием
     '''
-    _type = "particle" if randint(0,3)==0 else "ball"
+    _type = "particle" if randomReal(0,1) < particleProbability else "ball"
     rad = randomReal(R_MIN, R_MAX)
     x = randomReal(rad, SIZE_X - rad)
     y = randomReal(rad, SIZE_Y - rad)
-    ang = randomReal(0, 2*PI)
-    [vx, vy] = randomVelocity()
+    ang = randomReal(0, 2*pi)
+    if _type != "particle":
+        vx, vy = randomVelocity()
+    else:
+        vx, vy = randomReal(VPART_MIN,VPART_MAX), randomReal(VPART_MIN,VPART_MAX)
     color = randomColor(BALLCOLORS if _type=="ball" else PARTICLECOLORS)
     ltm = randomReal(0, LTM_MAX)
     return {
-            "pos": [x, y], 
-            "vel": [vx, vy],
+            "pos": (x, y), 
+            "vel": (vx, vy),
             "rad": rad, 
             "ltm": ltm,
             "col": color,
@@ -81,8 +81,8 @@ def drawBall(ball):
     '''
     Рисует шарик
     '''
-    (x, y) = ball["pos"]
-    (x, y) = (round(x), round(y))
+    x, y = ball["pos"]
+    x, y = round(x), round(y)
     rad = ball["rad"]
     col = ball["col"]
     _type = ball["type"]
@@ -91,11 +91,14 @@ def drawBall(ball):
     else:
         pygame.draw.polygon(screen, col, [(x-rad, y), (x, y+rad), (x+rad, y), (x, y-rad)])
 
-
 def drawBalls():
     '''
     Рисует все шарики из массива balls
     '''
+    f = pygame.font.Font(None, 30)
+    sur = f.render(str(int(score)), True, SCORECOLOR)
+    screen.blit(sur, (SIZE_X -  sur.get_size()[0], 0))
+ 
     for ball in balls:
         drawBall(ball)
 
@@ -112,48 +115,59 @@ def updateBallState(ball):
 
     if _type == "ball":
         pos = vecSum(pos, vecMul(vel, DT))
-        [x, y] = pos
-        [vx, vy] = vel
+        x, y = pos
+        vx, vy = vel
         v = (vx**2 + vy**2)**.5
         if x>=SIZE_X-rad and vx>0:
             x = SIZE_X-rad
-            da = PI/2 if vy<0 else 0
-            [vx, vy] = randomVelocity([v,v], [PI/2+da, PI+da])
+            da = pi/2 if vy<0 else 0
+            vx, vy = randomVelocity([v,v], [pi/2+da, pi+da])
 
         if x<=rad and vx<0:
             x = rad
-            da = PI/2 if vy>0 else 0
-            [vx, vy] = randomVelocity([v,v], [-PI/2+da, 0+da])
+            da = pi/2 if vy>0 else 0
+            vx, vy = randomVelocity([v,v], [-pi/2+da, 0+da])
 
         if y>=SIZE_Y-rad and vy>0:
             y = SIZE_Y-rad
-            da = PI/2 if vx>0 else 0
-            [vx, vy] = randomVelocity([v,v], [PI+da,3/2*PI+da])
+            da = pi/2 if vx>0 else 0
+            vx, vy = randomVelocity([v,v], [pi+da,3/2*pi+da])
 
         if y<=rad and vy<0:
             y = rad
-            da = PI/2 if vx<0 else 0
-            [vx, vy] = randomVelocity([v,v], [0+da,PI/2+da])
+            da = pi/2 if vx<0 else 0
+            vx, vy = randomVelocity([v,v], [0+da,pi/2+da])
 
-        pos = [x, y]
-        vel = [vx, vy]
+        pos = x, y
+        vel = vx, vy
     else:
-        pos = vecSum(pos, vecMul(randomVelocity([VPART, VPART]), DT))
-
-        [x, y] = pos
-        if x>=SIZE_X-rad:
+        x, y = pos
+        vx, vy = vel
+        v = (vx**2 + vy**2)**.5
+        oldvel = vel
+        vel = vx + vy * DT * MAGNETICFIELD * randomReal(.5, 1), vy - vx * DT * MAGNETICFIELD * randomReal(.5, 1)
+        pos = vecSum(pos, vecMul(vecMul(vecSum(oldvel, vel), 0.5), DT))
+        x, y = pos
+        vx, vy = vel
+        vx, vy = vecMul((vx, vy), 1 - DT * v/1000)
+        if x>=SIZE_X-rad and vx>0:
             x = SIZE_X-rad
+            vx *= -1
 
-        if x<=rad:
+        if x<=rad and vx<0:
             x = rad
+            vx *= -1
 
-        if y>=SIZE_Y-rad:
+        if y>=SIZE_Y-rad and vy>0:
             y = SIZE_Y-rad
+            vy *= -1
 
-        if y<=rad:
+        if y<=rad and vy<0:
             y = rad
+            vy *= -1
 
-        pos = [x, y]
+        pos = x, y
+        vel = vx, vy
     return {
         "type": _type,
         "pos": pos, 
@@ -174,12 +188,10 @@ def mouseButtonDown(event):
     '''
     Обрабатывает событие нажатия кнопки мыши
     '''
-    if event.button*0 != 1*0:
-        return
-    (x0, y0) = event.pos
+    x0, y0 = event.pos
     flag = False
     for i, ball in enumerate(balls):
-        (x, y) = ball["pos"]
+        x, y = ball["pos"]
         r = ball["rad"]
         _type = ball["type"]
         if (x - x0)**2 + (y - y0)**2 <= r**2:
@@ -195,12 +207,11 @@ def dScore(r, _type):
     '''
     Приращение количества очков за нажатие на шарик
     r - радиус шарика
-
     '''
     if _type == "ball":
         return SIZE_X/NUMBER_OF_BALLS*V_MAX / (r+1)
     else:
-        return SIZE_X/NUMBER_OF_BALLS*VPART / (r+1)
+        return SIZE_X/NUMBER_OF_BALLS*VPART_MAX / (r+1)
 
 def updateScore(newScore):
     '''
@@ -215,10 +226,6 @@ def showScore():
     '''
     print(score)
 
-PI = math.pi
-sin = math.sin
-cos = math.cos
-
 pygame.init()
 SIZE_X, SIZE_Y = 500, 500
 
@@ -226,7 +233,7 @@ FPS = 100
 DT = 1/FPS
 time = 0
 
-RED = (255, 0, 0,)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -234,19 +241,22 @@ CYAN = (0, 255, 255)
 MAGENTA = (255, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+DARKGRAY = (63, 63, 63)
 BALLCOLORS = [RED, GREEN, BLUE, YELLOW, CYAN, MAGENTA]
-PARTICLECOLORS = [WHITE]
+SCORECOLOR = (0, 255, 0)
+PARTICLECOLORS = [WHITE, DARKGRAY]
 
 score = 0
 MISCLICK_FINE = 50
-
+MAGNETICFIELD = 10
+particleProbability = .2
 R_MIN, R_MAX = 20, 50
-V_MIN, V_MAX = 30, 100
+V_MIN, V_MAX = 100, 300
 LTM_MAX = 1000
-VPART = 300
+VPART_MIN, VPART_MAX = 100, 300
 
 NUMBER_OF_BALLS = 10
-balls = [] * NUMBER_OF_BALLS
+balls = []
 
 for i in range(0, NUMBER_OF_BALLS):
     balls.append(randomBallState())
@@ -264,7 +274,7 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouseButtonDown(event)
     time += DT
-    title = "Score: " + str(round(score)) + "  Score per second: " + str(round(score/time, 1))
+    title = "Score: " + str(round(score)) + "  Score per second: " + str(round(score / time, 1))
     pygame.display.set_caption(str(title))
     updateBallStates()
     drawBalls()
